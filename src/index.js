dc214c9cca9581226db1370df884efabdc49ae13
DariaShibalko <daryaavramo@yandex.by>
2021-02-26 16:11:38 +0300

    feat: done typical arrays problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if(arr == null || arr.length == 0 ){
+    return 0;
+  } 
+  else{
+    let mini=arr[0];
+    let temp;
+    for (let i=1;i<arr.length;i++){
+      if(mini>arr[i]){
+        temp=mini;
+        mini=arr[i];
+        arr[i]=temp;
+      }
+      }
+      return mini;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if(arr == null || arr.length == 0 ){
+    return 0;
+  } 
+  let maxi=arr[0];
+  let temp;
+  for (let i=1;i<arr.length;i++){
+    if(maxi<arr[i]){
+      temp=maxi;
+      maxi=arr[i];
+      arr[i]=temp;
+    }
+  }
+  return maxi;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if(arr == null || arr.length == 0 ){
+    return 0;
+  } 
+  let sum=0;
+  for (let i=0;i<arr.length;i++){
+    sum+=arr[i];
+  }
+  return sum/arr.length;
 }
